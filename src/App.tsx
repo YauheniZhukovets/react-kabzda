8a0daeb2c317af03fae352ad4aa1bd04582cf2f4
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-11-09 20:39:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./Componets/Accardion/Accordion";
+import {Rating} from "./Componets/Rating/Rating";
+
+// function declaration
+function App() {
+    console.log("App rendering")
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"MENU"} collapsed={true}/>
+            <Accordion titleValue={"USERS"} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1>{ props.title }</h1>
+
+}
+
+
+
+export default App;
